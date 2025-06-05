7302e2faac8a2b3325e0255e8dc90ad407f528dd
filmbythomas <filmbythomas@gmail.com>
2025-06-05 17:08:25 -0400

    npx convex dev ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -17,6 +17,7 @@ import type * as auth from "../auth.js";
 import type * as bookings from "../bookings.js";
 import type * as emailActions from "../emailActions.js";
 import type * as http from "../http.js";
+import type * as likeImage from "../likeImage.js";
 import type * as portfolio from "../portfolio.js";
 import type * as router from "../router.js";
 
@@ -33,6 +34,7 @@ declare const fullApi: ApiFromModules<{
   bookings: typeof bookings;
   emailActions: typeof emailActions;
   http: typeof http;
+  likeImage: typeof likeImage;
   portfolio: typeof portfolio;
   router: typeof router;
 }>;
