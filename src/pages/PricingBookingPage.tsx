20771c8e3aa8c8bd42d3e84be53496653cd67f46
filmbythomas <filmbythomas@gmail.com>
2025-06-04 23:54:10 -0400

    fix error

--- src/pages/PricingBookingPage.tsx
+++ src/pages/PricingBookingPage.tsx
@@ -288,7 +288,7 @@ const portraitsTiers: Tier[] = [
       "3-hour immersive session",
       "60+ edited photos",
       "Up to 3 locations",
-      "3 Outfit changes included"
+      "3 Outfit changes included",
       "5 8x10 photos",
       "Location Consultation",
       "All image files",
